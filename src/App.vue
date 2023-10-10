8b2c952aff36ac9df5202498036fd2e0933313e6
Joao Vitor Ribeiro <joaovitordias.2b@gmail.com>
2023-10-09 22:03:54 -0300

    Comitando

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<template>
+  <div class="container">
+    <header class="p-5 mb-4 mt-4 bg-ligth roudend-3">
+      <h1>Minhas tarefas</h1>
+      <p>
+        Você possui 4 tarefas pendente
+      </p>
+    </header>
+    <form >
+    <div class="row">
+      <div class="col">
+        <input type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
+      </div>
+      <div class="col-md-1">
+        <button type="submit" class="btn btn-primary">
+          Cadastrar
+        </button>
+      </div>
+      <div class="col-md-2">
+        <select class="form-control">
+          <option value="todas">Todas tarefas</option>
+          <option value="todas">Tarefas Pendentes</option>
+          <option value="todas">Tarefas Finalizadas</option>
+        </select>
+      </div>
+    </div>
+  </form>
+  </div>
+
+</template>
+
+
+<style scoped>
 </style>
